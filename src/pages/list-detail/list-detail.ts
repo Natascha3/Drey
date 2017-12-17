16d281da46356d28a48016840c2462ab1c4e828f
Daniel Brommer <d.brommer@micromata.de>
2017-12-17 20:20:27 +0100

    Add detail

---
+++ src/pages/list-detail/list-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-list-detail',
+  templateUrl: 'list-detail.html',
+})
+export class ListDetailPage {
+
+  constructor() {}
+
+}
--- src/pages/list/list-overview/list-overview.ts
+++ src/pages/list/list-overview/list-overview.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { NewListPage } from './newList/newList';
 
 @Component({
   selector: 'page-listOverview',
@@ -6,10 +8,36 @@ import { Component } from '@angular/core';
 })
 export class ListOverviewPage {
 
-  constructor() {
+  items = [
+    'Pokémon Yellow',
+    'Super Metroid',
+    'Mega Man X',
+    'The Legend of Zelda',
+    'Pac-Man',
+    'Super Mario World',
+    'Street Fighter II',
+    'Half Life',
+    'Final Fantasy VII',
+    'Star Fox',
+    'Tetris',
+    'Donkey Kong III',
+    'GoldenEye 007',
+    'Doom',
+    'Fallout',
+    'GTA',
+    'Halo'
+  ];
+
+  constructor(public navCtrl: NavController) {
   }
 
+  itemSelected(item) {
+    console.log(item);
+  }
 
+  addList() {
+    this.navCtrl.push(NewListPage);
+  }
 }
 
 
